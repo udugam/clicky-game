e05fa572bda316b8e7ac28002b76afbc987eb74b
udugam <ryan.udugam@gmail.com>
2018-10-16 22:43:07 -0400

    Updates

---
+++ precache-manifest.8f50cd92941589f52c86c643df6385f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "cc95c5cca3dccb9b3e48",
+    "url": "/clicky-game/static/js/main.cc95c5cc.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/clicky-game/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "cc95c5cca3dccb9b3e48",
+    "url": "/clicky-game/static/css/main.6508d288.chunk.css"
+  },
+  {
+    "revision": "3a344abcc1a1c9c9c06f7f0de72129a1",
+    "url": "/clicky-game/index.html"
+  }
+];
