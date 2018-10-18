2f8de0596ff2c6888ab33665e952315298926ccc
udugam <ryan.udugam@gmail.com>
2018-10-18 01:58:30 -0400

    added playing card images

--- clicky/src/App.js
+++ clicky/src/App.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
-import {PageHeader} from 'react-bootstrap'
+import {PageHeader, Grid, Row, Col} from 'react-bootstrap'
+import cards from './cards.json'
+import Card from './Components/Card'
 
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <PageHeader>
-          The Clicky Game! <small>Click on each image exactly once to win!</small>
-        </PageHeader>
-
-      </div>
+      <Grid>
+        <Row>
+          <PageHeader>
+            The Clicky Game! <small>Click on each image exactly once to win!</small>
+          </PageHeader>
+        </Row>
+        <Row>
+          {cards.map(card=>(
+            <Col xs={6} md={4}>
+              <Card playingCard={card} />
+            </Col>
+          ))}
+        </Row>
+      </Grid>
     );
   }
 }
 
 export default App;
+
---
+++ clicky/src/Components/Card.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import {Image} from 'react-bootstrap'
+
+class Card extends Component {
+    state = {
+        clicked: false
+    }
+
+    render(props) {
+        return(
+            <Image src={this.props.playingCard.imagePath} alt="playing Card"/> 
+        )
+    }
+}
+export default Card
