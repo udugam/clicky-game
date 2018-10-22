0a20c38cbe589ff9e14425b1b6d8a2a1c644977a
udugam <ryan.udugam@gmail.com>
2018-10-22 13:00:15 -0400

    added styling to score board component

--- clicky/src/Components/ScoreBoard.js
+++ clicky/src/Components/ScoreBoard.js
@@ -2,9 +2,19 @@ import React from 'react'
 
 const ScoreBoard = props=> (
     <div>
-        <h1>Score: {props.state.score}</h1>
-        <h1>HighScore: {props.state.highScore}</h1>
+        <span style={styles.score}>Score: {props.state.score}</span>
+        <span style={styles.highScore}>HighScore: {props.state.highScore}</span>
     </div>
 )
+
+const styles= {
+    score: {
+        fontSize:'30px'
+    },
+    highScore: {
+        color:'blue',
+        float:'right'
+    }
+}
 
 export default ScoreBoard
